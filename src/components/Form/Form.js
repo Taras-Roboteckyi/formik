05c04559f902de22ebab4b076c9b4927d476e682
Taras-Roboteckyi <rtvr@ukr.net>
2022-07-09 21:08:07 +0300

    add Formik

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,73 @@
-import { useState } from "react";
-import { nanoid } from "nanoid";
+//import { useState } from "react";
+//import { nanoid } from "nanoid";
+import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 import {
   FormPhoneBook,
   LabelPhoneBook,
   InputPhoneBook,
   ButtonPhoneBook,
+  ErrorPhoneBook,
 } from "./Form.styled";
 
 export default function ContactForm({ formSubmit }) {
+  return (
+    <Formik
+      initialValues={{ name: "", number: "" }}
+      validationSchema={Yup.object({
+        name: Yup.string()
+          .min(2, "Too Short!")
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
+        number: Yup.number().min(2, "Too Short!").required("Required"),
+      })}
+      onSubmit={(values, { setSubmitting, resetForm }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          formSubmit(values);
+          setSubmitting(false);
+
+          resetForm({ name: "", number: "" });
+        }, 400);
+      }}
+    >
+      {(formik) => (
+        <FormPhoneBook>
+          <LabelPhoneBook htmlFor="name">Name</LabelPhoneBook>
+          <InputPhoneBook
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$" //Без цього патерна не працює))))))))
+            type="text"
+            autoComplete="off"
+            placeholder="Jacob Mercer"
+            {...formik.getFieldProps("name")}
+          />
+          {formik.touched.name && formik.errors.name ? (
+            <ErrorPhoneBook>{formik.errors.name}</ErrorPhoneBook>
+          ) : null}
+
+          <LabelPhoneBook htmlFor="number">Number</LabelPhoneBook>
+          <InputPhoneBook
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            type="text"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            autoComplete="off"
+            placeholder="444-43-45"
+          />
+          {formik.touched.number && formik.errors.number ? (
+            <ErrorPhoneBook>{formik.errors.number}</ErrorPhoneBook>
+          ) : null}
+
+          <ButtonPhoneBook type="submit">Add contact</ButtonPhoneBook>
+        </FormPhoneBook>
+      )}
+    </Formik>
+  );
+}
+/* export default function ContactForm({ formSubmit }) {
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
 
@@ -78,4 +137,4 @@ export default function ContactForm({ formSubmit }) {
       <ButtonPhoneBook type="submit">Add contact</ButtonPhoneBook>
     </FormPhoneBook>
   );
-}
+} */
--- src/components/Form/Form.styled.jsx
+++ src/components/Form/Form.styled.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import { Field, Form } from "formik";
 
-export const FormPhoneBook = styled.form`
+export const FormPhoneBook = styled(Form)`
   border-color: black;
   border-style: solid;
   padding: 20px;
@@ -12,22 +13,38 @@ export const FormPhoneBook = styled.form`
   border-radius: 4px;
 `;
 export const LabelPhoneBook = styled.label`
-  padding-bottom: 20px;
+  padding-bottom: 0px;
   display: flex;
   flex-direction: column;
   font-size: 15px;
   font-weight: 500;
 `;
 
-export const InputPhoneBook = styled.input`
+export const InputPhoneBook = styled(Field)`
   margin-top: 5px;
   padding-left: 10px;
   font-size: 15px;
   font-weight: 400;
+  border: 2px solid #edf2f7;
+  background-color: #f7fafc;
+  color: #2d3748;
+  border-radius: 10px;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  &:focus {
+    outline: none;
+    border: 2px solid #4299e1;
+  }
+  &:invalid {
+    border: 2px solid #ff7d87;
+    box-shadow: none;
+  }
 `;
 
 export const ButtonPhoneBook = styled.button`
   display: inline-flex;
+  margin-top: 10px;
   padding: 5px 20px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
   font-size: 16px;
@@ -41,4 +58,10 @@ export const ButtonPhoneBook = styled.button`
     background-color: royalblue;
     transition: background-color 500ms cubic-bezier(0.4, 0, 0.2, 1);
   }
+`;
+export const ErrorPhoneBook = styled.div`
+  font-size: 12px;
+  color: #e53e3e;
+
+  margin-top: 0.25rem;
 `;
